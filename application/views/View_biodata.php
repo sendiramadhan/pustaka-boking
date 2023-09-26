22bf2e139e32a9f8df454430c327b0c5815e69a4
sendiramadhan <sendiramadhan@gmail.com>
2023-09-26 15:48:16 +0700

    Tugas Biodata

---
+++ application/views/View_biodata.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to Sendi Ramadhan!</h1>
+
+	<div id="body">
+		<p>Biodata ku</p>
+
+        <p>Nama : Sendi Ramadhan<p>
+        <P>kelas : 10.3A.07<p>
+		<p>Jurusan : Rekayasa Perangkat Lunak</p>
+		<p>Alamat : Johar baru</p>
+		<p>No.Hp :083121869035</p>
+	</div>
+
+	<p class="footer">
+</div>
+
+</body>
+</html>

989a994711b7612d580eff0018e4ed740c2d3909
sendiramadhan <sendiramadhan@gmail.com>
2023-10-03 19:43:20 +0700

    tugas pertemuan 3

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,17 @@
+<?php
+Class Matakuliah extends CI_Controller {
+
+    public function index() {
+        $this->load->view('View-form-matakuliah');
+    }
+
+    public function cetak() {
+        $data = [
+            'kode' => $this->input->post('kode'),
+            'nama' => $this->input->post('nama'),
+            'sks' => $this->input->post('sks'),
+        ];
+
+        $this->load->view('View-data-matakuliah', $data);
+    }
+}

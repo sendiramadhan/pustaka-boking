af47ecb9fac2d2d2342a0fba29516ce9ff8d7a11
sendiramadhan <sendiramadhan@gmail.com>
2023-10-18 21:08:43 +0700

    tugas Ke 5

---
+++ application/views/v_index.php
@@ -0,0 +1,14 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p align='justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Provident, delectus. Corrupti fugit libero pariatur vel!
+         Ratione maxime, neque voluptate magni ducimus nihil architecto?
+         Commodi et modi facilis vitae nesciunt veritatis?</p>
+         <ol type="a">
+    <li>Model</li>
+    <p align='justify'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Tenetur eveniet et ipsa sequi, magni commodi fuga incidunt dignissimos 
+        dicta officiis cupiditate numquam error quod accusamus qui ea rem nisi ducimus?</p>
+    <li>Controller</li>
+    <p align='justify'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+</section>

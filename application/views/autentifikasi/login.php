d4a9f1e7939da227e3c1234efe97c48a91ee3018
sendiramadhan <sendiramadhan@gmail.com>
2023-11-21 15:26:11 +0700

    upload hari ini

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -14,37 +14,24 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Halaman Login!</h1>
                                 </div>
-                                <form class="user">
+                                <?= $this->session->flashdata('pesan'); ?>
+                                <form class="user" method="post" action="<? base_url('autentifikasi'); ?>">
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                        <input type="text" class="form-control form-control-user" Value="<?= set_value('email'); ?>
+                                        " id="email" placeholder="Masukan Alamat Email" name="email">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                        <input type="password" class="form-control form-control-user" id="Password" placeholder="Password" name="password">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">Remember
-                                                Me</label>
-                                        </div>
-                                    </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </a>
-                                    <hr>
-                                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                                        <i class="fab fa-google fa-fw"></i> Login with Google
-                                    </a>
-                                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                    </a>
+                                    <button type="submit" class="btn btn-primary btn-user
+                                    btn-block">masuk</button>
                                 </form>
-                                <hr>
+                                <br>
                                 <div class="text-center">
-                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                </div>
-                                <div class="text-center">
-                                    <a class="small" href="register.html">Create an Account!</a>
+                                    <a class="small" href="<?= base_url('autentifikasi/registrasi'); ?>">Daftar Member!</a>
+
                                 </div>
                             </div>
                         </div>
